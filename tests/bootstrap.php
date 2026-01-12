f3ad3cb776db22371e23c46c6083254ee4cc335b
Ahmed Shaban <ahmedshaban33ah@gmail.com>
2026-01-12 15:16:06 +0200

    test: Add test bootstrap file

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2026 OurEdu
+ * Multi-Tenant Infrastructure for Laravel Services
+ */
+
+// Load Composer autoloader
+require_once __DIR__.'/../vendor/autoload.php';
+
